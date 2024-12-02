b90307d5ce9a578314814499403061e3cf2e9b2a
lukkas06 <lucas.p2006@aluno.ifsc.edu.br>
2024-12-02 16:18:31 -0200

    Update exercicios_praticos.js

--- JavaScript/exercicios_praticos.js
+++ JavaScript/exercicios_praticos.js
@@ -1,12 +1,1 @@
-/* Questão 2
-Crie um programa que conte os números de 1 até 10, e os imprima na console.
-Utilize todas as estruturas de repetição disponíveis no JavaScript.*/
-for(let controlDoLacoFor = 1; controleDoLacoFor<=10; controleDoLacoFor){   
-}
-console.log("\n");
-const NUMERO_MAXIMO_ITERACORES_LACO_WHILE=10
-let controlDoLacoWhile = 1;
-while(controlDoLacoWhile<= NUMERO_MAXIMO_ITERACORES_LACO_WHILE){
-    console.log("laço de repetição while exibino o numero:",controlDoLacoWhile);
-    controlDoLacoWhile++;
-}
+array
